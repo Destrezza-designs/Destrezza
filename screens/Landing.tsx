2872b4771fa40c7a9c3cabd6a08b619c66d2d04e
Milanjiji <milanjiji0987654321@gmail.com>
2025-08-29 09:44:36 +0530

    loading in the start

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Main from './home/Main'
 import Products from './home/Products'
 import ProductsShowcase from './home/ProductsShowcase'
 import Visit from './home/Visit'
 import OurLogo from './home/OurLogo'
 import Footor from '@/components/Footor'
+import FullPageLoader from '@/components/utils/FullPageLoader'
 
 const Landing = () => {
+
+  const [loading,setLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+    },5000)
+  },[])
   return (
     <div>
+
+        {loading && <FullPageLoader />}
+
         <Main />
         <Products />
         <ProductsShowcase />
